8d6785b962e3fe1d7d4db1023482d596cb05ead3
Felipe Chitz <felipe.chitz@lactec.com.br>
2026-01-13 10:22:47 -0300

    Exercício 13 desafio 1: add vlibras

--- frontend/packages/volto-lactec-intranet/src/index.ts
+++ frontend/packages/volto-lactec-intranet/src/index.ts
@@ -1,8 +1,17 @@
 import type { ConfigType } from '@plone/registry';
 import installSettings from './config/settings';
+import Libras from '@plonegovbr/volto-vlibras/components/Libras';
 
 function applyConfig(config: ConfigType) {
   installSettings(config);
+
+  config.settings.appExtras = [
+    ...config.settings.appExtras,
+    {
+      match: '',
+      component: Libras,
+    },
+  ];
 
   return config;
 }
