0f5ac16e35aabd2e356ec3aaa330d50eaac89bd2
Felipe Chitz <felipe.chitz@lactec.com.br>
2026-01-13 11:00:25 -0300

    Exercício 17: Testes de configuração do backend

---
+++ backend/tests/setup/test_portal_setttings.py
@@ -0,0 +1,27 @@
+"""Portal settings tests."""
+
+from plone import api
+
+import pytest
+
+
+class TestPortalSettings:
+    """Test that Portal configuration is correctly done."""
+
+    @pytest.mark.parametrize(
+        "key,expected",
+        [
+            ["plone.site_title", "Intranet da Lactec"],
+            ["plone.email_from_name", "Intranet da Lactec"],
+            ["plone.smtp_host", "localhost"],
+            ["plone.smtp_port", 25],
+            ["plone.available_timezones", ["America/Sao_Paulo"]],
+            ["plone.portal_timezone", "America/Sao_Paulo"],
+            ["plone.navigation_depth", 4],
+            ["plone.twitter_username", "plone"],
+        ],
+    )
+    def test_setting(self, portal, key: str, expected: str | int):
+        """Test registry setting."""
+        value = api.portal.get_registry_record(key)
+        assert value == expected
