7752d3cbb47c391bcfc6c7b6a91c2c6e572cd1df
Felipe Chitz <felipe.chitz@lactec.com.br>
2026-01-13 14:59:03 -0300

    Exercício 23: Visão para o tipo Área

--- frontend/packages/volto-lactec-intranet/src/index.ts
+++ frontend/packages/volto-lactec-intranet/src/index.ts
@@ -1,9 +1,11 @@
 import type { ConfigType } from '@plone/registry';
 import installSettings from './config/settings';
 import Libras from '@plonegovbr/volto-vlibras/components/Libras';
+import installViews from './config/views';
 
 function applyConfig(config: ConfigType) {
   installSettings(config);
+  installViews(config);
 
   config.settings.appExtras = [
     ...config.settings.appExtras,
