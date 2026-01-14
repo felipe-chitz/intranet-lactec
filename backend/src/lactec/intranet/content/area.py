40e794666ae6287c041b1d9ada3d5b69ce9ad578
Felipe Chitz <felipe.chitz@lactec.com.br>
2026-01-14 10:34:34 -0300

    Exercício 30: Cria behavior contato e endereco

--- backend/src/lactec/intranet/content/area.py
+++ backend/src/lactec/intranet/content/area.py
@@ -1,74 +1,10 @@
-from lactec.intranet import _
-from lactec.intranet.utils import validadores
 from plone.dexterity.content import Container
-from plone.schema.email import Email
 from plone.supermodel import model
-from zope import schema
 from zope.interface import implementer
 
 
 class IArea(model.Schema):
     """Definição de uma Área."""
-
-    model.fieldset(
-        "contato",
-        _("Contato"),
-        fields=[
-            "email",
-            "telefone",
-        ],
-    )
-
-    model.fieldset(
-        "endereco",
-        _("Endereço"),
-        fields=[
-            "endereco",
-            "complemento",
-            "cidade",
-            "estado",
-            "cep",
-        ],
-    )
-
-    email = Email(
-        title=_("Email"),
-        required=True,
-        constraint=validadores.is_valid_email,
-    )
-
-    telefone = schema.TextLine(
-        title=_("Telefone"),
-        description=_("Informe o telefone de contato"),
-        required=False,
-        constraint=validadores.is_valid_telefone,
-    )
-
-    endereco = schema.TextLine(
-        title=_("Endereço"),
-        required=False,
-    )
-
-    complemento = schema.TextLine(
-        title=_("Complemento"),
-        required=False,
-    )
-
-    cidade = schema.TextLine(
-        title=_("Cidade"),
-        required=False,
-    )
-
-    estado = schema.Choice(
-        title=_("Estado"),
-        vocabulary="lactec.intranet.vocabulary.estados",
-        required=False,
-    )
-
-    cep = schema.TextLine(
-        title=_("Cep"),
-        required=False,
-    )
 
 
 @implementer(IArea)
