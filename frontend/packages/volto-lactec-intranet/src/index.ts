d54ef249b6431be229f906ade3739935c32d920a
felipe-chitz <felipe.chitz@lactec.com.br>
2026-01-21 17:27:11 -0300

    Exercício 49: block clima dinâmico

--- frontend/packages/volto-lactec-intranet/src/index.ts
+++ frontend/packages/volto-lactec-intranet/src/index.ts
@@ -3,11 +3,13 @@ import installSettings from './config/settings';
 import Libras from '@plonegovbr/volto-vlibras/components/Libras';
 import installViews from './config/views';
 import installBlocks from './config/blocks';
+import installReducers from 'volto-lactec-intranet/config/reducers';
 
 function applyConfig(config: ConfigType) {
   installSettings(config);
   installViews(config);
   installBlocks(config);
+  installReducers(config);
 
   config.settings.appExtras = [
     ...config.settings.appExtras,
